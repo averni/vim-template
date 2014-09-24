687f72bd713344656bbe075ac5d0c4998c2678d7
antonio <me.verni@gmail.com>
2014-09-24 13:52:10 +0200

    isf openerp template

---
+++ templates/=template=isf_*.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © %YEAR% %USER% <%MAIL%>
+#
+# Distributed under terms of the %LICENSE% license.
+
+"""
+"""
+
+from openerp.osv import osv, fields, orm
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class %FILE%:
+    _name = "%OPENERPCLASS%"
+    _description = ""
+    _columns = {
+        %HERE%
+    }
